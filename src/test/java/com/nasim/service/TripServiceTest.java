8fb2422b38f7e226e9271c69084e58292a04bf2f
nasimakhtar <nasim@backbase.com>
2021-01-19 23:54:31 +0800

    created test class

---
+++ src/test/java/com/nasim/service/TripServiceTest.java
@@ -0,0 +1,10 @@
+package com.nasim.service;
+
+import org.junit.Test;
+
+public class TripServiceTest {
+    @Test
+    public void test_trip_service() {
+        TripService tripService = new TripService();
+    }
+}
